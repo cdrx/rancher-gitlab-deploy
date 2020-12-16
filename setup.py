ec925a62437a766d6a64db36ffbfb53f6e7112bb
cdrx <cdrx@users.noreply.github.com>
2020-12-16 13:40:50 +0000

    chore: version bump

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup
 
-setup(name='rancher-gitlab-deploy',
-    version='1.5',
-    description='Command line tool to ease updating services in Rancher from your GitLab CI pipeline',
-    url='https://github.com/cdrx/rancher-gitlab-deploy',
-    author='cdrx',
-    license='MIT',
-    packages=['rancher_gitlab_deploy'],
+setup(
+    name="rancher-gitlab-deploy",
+    version="1.7",
+    description="Command line tool to ease updating services in Rancher from your GitLab CI pipeline",
+    url="https://github.com/cdrx/rancher-gitlab-deploy",
+    author="cdrx",
+    license="MIT",
+    packages=["rancher_gitlab_deploy"],
     zip_safe=False,
     install_requires=[
-        'click',
-        'requests',
-        'colorama'
+        "click",
+        "requests",
+        "colorama",
     ],
-    entry_points = {
-        'console_scripts': ['rancher-gitlab-deploy=rancher_gitlab_deploy.cli:main'],
-    }
+    entry_points={
+        "console_scripts": [
+            "rancher-gitlab-deploy=rancher_gitlab_deploy.cli:main",
+        ],
+    },
 )
